285427cd09b587d2337015f06134a60f507f398d
dayo_aj <ifedayo.ajibola@outlook.com>
2019-10-03 09:40:38 +0100

    Add details of Profile

---
+++ src/initial-data.js
@@ -0,0 +1,48 @@
+const initialData = {
+  staffs: {
+    "staff-1": {
+      EmployeeID: "staff-1",
+      FirstName: "Temidayo Akerele",
+      Position: "Officer II",
+      Department: "Corporate and Strategic Planning",
+      PhoneNumber: "08046456456"
+    },
+    "staff-2": {
+      EmployeeID: "staff-2",
+      FirstName: "Moti Terngu",
+      Position: "Prinicipal Manager",
+      Department: "Corporate and Strategic Planning",
+      PhoneNumber: "080557456456"
+    },
+    "staff-3": {
+      EmployeeID: "staff-3",
+      FirstName: "Ali Mohammed",
+      Position: "System Analyst I",
+      Department: "ICT",
+      PhoneNumber: "080245410546"
+    },
+    "staff-4": {
+      EmployeeID: "staff-4",
+      FirstName: "Esther Timothy",
+      Position: "Senior Manager",
+      Department: "ICT",
+      PhoneNumber: "080304556456"
+    },
+    "staff-5": {
+      EmployeeID: "staff-5",
+      FirstName: "Ruth Yankari",
+      Position: "Officer I",
+      Department: "Corporate and Strategic Planning",
+      PhoneNumber: "080666645236"
+    },
+    "staff-6": {
+      EmployeeID: "staff-6",
+      FirstName: "Ayobami Taiwo",
+      Position: "General Manager",
+      Department: "Corporate and Strategic Planning",
+      PhoneNumber: "080464555516"
+    }
+  }
+};
+
+export default initialData;
